f558d77d7896803f2978dfac8d279f8b4ff91a92
Priya Kumar <priya.kumar08@yahoo.com>
2016-07-21 15:45:22 +0000

    recovers model

---
+++ models/model.rb
@@ -0,0 +1,40 @@
+# def size(size,event)
+#
+# if size== "S" && event== "A"
+#   puts "Hey #{user}! You mean business? This is what we recommend:"
+# elsif size== "M" && event== "A"
+#   puts "Hey #{user}! You mean business? This is what we recommend:"
+# elsif size=="L" && event=="A"
+#   puts "Hey #{user}! You mean business? This is what we recommend:"
+# elsif size=="S" && event=="B"
+#   puts "Hey #{user}! You're feelin' fancy? This is what we recommend:"
+# elsif size=="M"&& event=="B"
+#   puts "Hey #{user}! You're feelin' fancy? This is what we recommend:"
+# elsif size=="L" && event=="B"
+#   puts "Hey #{user}! You're feelin' fancy? This is what we recommend:"
+# elsif size "S" && event=="C"
+#   puts "Hey #{user}! It's a casual day? This is what we recommend:"
+# elsif size "M" && event=="C"
+#   puts "Hey #{user}! It's a casual day? This is what we recommend:"
+# elsif size "L" && event=="C"
+#   puts "Hey #{user}! It's a casual day? This is what we recommend:"
+#   end
+# end
+
+def result(size,event)
+if size=="s"
+  if event=="business"
+    "Hello"
+  end
+end
+if size=="m"
+  if event =="business"
+    "Hey!"
+  end
+end
+if size "l"
+  if event =="business"
+    "HEELLLOOO"
+  end
+end
+end
